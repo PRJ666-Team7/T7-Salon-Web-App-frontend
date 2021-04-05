b4882878b446867ea760b36cac839e760af3e52a
unknown <jaspermui@hotmail.ca>
2021-04-05 05:47:20 -0400

    config

--- src/components/helpers/Config.js
+++ src/components/helpers/Config.js
@@ -1,9 +1,9 @@
 class Config {
     //hosting
-    // api = "https://salon-api.herokuapp.com"
+    api = "https://salon-api.herokuapp.com"
 
     //dev
-    api = 'http://localhost:8000'
+    // api = 'http://localhost:8000'
 }
 
 export default new Config()
